3c31bff7766cfb88637571d278614ac7b17a3b9b
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-09-28 18:08:24 +0300

    test pathes

--- src/js/pages/homePage/index.js
+++ src/js/pages/homePage/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import withStore from '../../hocs/withStore'
 
 function homePage() {
-	return (
-		<div>HomePage</div>
-	)
+  return (
+    <div>HomePage
+      <img src={'ReactAppStore/dist/assets/imgs/televisions/samsung/T32E310EX/front.webp'} />
+      <img src={'ReactAppStore/dist/assets/imgs/televisions/samsung/T32E310EX/front.png'} />
+    </div>
+  )
 }
 
 export default withStore(homePage)

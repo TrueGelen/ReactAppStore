a091552adcf8cc05bb13dc6a5749c43fd07a6f49
Vlad Filchgin <v.filchagin@infomaximum-llc.ru>
2021-08-31 17:50:23 +0300

    chore: updated swiper

--- src/js/stores/index.js
+++ src/js/stores/index.js
@@ -19,21 +19,21 @@ class RootStore {
 		}
 
 		//dev base url
-		/* this.baseUrlImgs = {
-			phones: '/assets/imgs/phones/',
-			televisions: '/assets/imgs/televisions/',
-			tablets: '/assets/imgs/tablets/'
-		} */
+		this.baseUrlImgs = {
+			phones: '/assets/images/phones/',
+			televisions: '/assets/images/televisions/',
+			tablets: '/assets/images/tablets/'
+		}
 
 		// todo: наверняка можно сделать baseUrl = ReactAppStore/dist/assets/ и функцию которая бы клеила остатки к этому адресу.
 		// и с помощью webpack'а менять этот baseUrl на /assets/ в зависимости от команды для сборки
 
 		//dist base url. for gitHub page
-		this.baseUrlImgs = {
-			phones: 'ReactAppStore/dist/assets/imgs/phones/',
-			televisions: 'ReactAppStore/dist/assets/imgs/televisions/',
-			tablets: 'ReactAppStore/dist/assets/imgs/tablets/'
-		}
+		/* this.baseUrlImgs = {
+			phones: 'ReactAppStore/dist/assets/images/phones/',
+			televisions: 'ReactAppStore/dist/assets/images/televisions/',
+			tablets: 'ReactAppStore/dist/assets/images/tablets/'
+		} */
 
 		this.localStorage = localStorage
 		this.cart = new cartStore(this)
